4af7871a46cc5f3a4d635f81a790d4a9c9bbebd5
Davide Fucci <dfucci@users.noreply.github.com>
2016-11-24 15:22:34 +0100

    Update SudokuVerifier.java

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -3,7 +3,11 @@ package org.univoulu.tol.sqatlab.sudoku;
 public class SudokuVerifier {
 
 	public int verify(String candidateSolution) {
-		// returns 1 if the candidate solution is correct
+		// Return 0: means it is a valid Sudoku solution
+		// Return -1: means it is violating Rule #1
+		// Return -2: means it is violating Rule #2
+		// Return -3: means it is violating Rule #3
+		// Return -4: means it is violating Rule #4
 		return 0;
 	}
 }
