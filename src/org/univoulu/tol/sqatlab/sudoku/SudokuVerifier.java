8996015c5ca4557b68e665c42ea89ccaae1bc3c1
Korisnik <Korisnik@Asus-Laptop>
2017-06-08 17:26:08 +0200

    Done.

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -8,6 +8,224 @@ public class SudokuVerifier {
 		// Return -2: means it is violating Rule #2
 		// Return -3: means it is violating Rule #3
 		// Return -4: means it is violating Rule #4
+		
+		
+		// Splitting the forwarded string into rows of 9 elements
+		String[] rows = SplitRows(candidateSolution);
+		
+		// Splitting the forwarded string into columns of 9 elements
+		String[] columns = SplitColumns(candidateSolution);
+	
+	
+		// Iterating through each string to see if there is a digit which is not 1-9
+		// If there is a digit which is not 1-9, the verify function will return -1 as per instructions
+		for (int i = 0; i < rows.length; i++)
+		{
+			char[] row = rows[i].toCharArray();
+			
+			for(int j = 0; j < row.length; j++)
+			{
+				if(Character.getNumericValue(row[j]) < 1 || Character.getNumericValue(row[j]) > 9)
+					return -1;
+			}
+		}
+		
+		
+		// Iterating through each sub-grid to see if there are two same digits
+		// If there are, the verify function will return -2 as per instructions
+		
+		
+		// Initializing the mainGrid (9x9)
+		int[][] mainGrid = new int[9][9];
+		
+		for (int i = 0; i < 9; i++)
+		{
+			char[] rowsElements = rows[i].toCharArray();
+			
+			for (int j = 0; j < 9; j++)
+			{	
+				mainGrid[i][j] = Character.getNumericValue(rowsElements[j]);
+			}
+		}
+		
+		///////////PRINTING//////////
+		for (int i = 0; i < 9; i++)
+		{
+			for (int j = 0; j < 9; j++)
+			{	
+				System.out.print(mainGrid[i][j] + "  ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+		///////////PRINTING//////////
+		
+		
+		// Looping through the mainGrid and creating the sub-grids (3x3)
+		
+		int[][] subGrid = new int[3][3];		
+		
+		for(int i = 0; i < 9; i+=3)
+		{
+			for(int j = 0; j < 9; j+=3)
+			{
+				for (int k = 0; k < 3; k++)
+				{	
+					for (int l = 0; l < 3; l++)
+					{	
+						subGrid[k][l] = mainGrid[i+k][j+l];					
+					}
+				}
+				
+				///////////PRINTING//////////
+				for (int m = 0; m < 3; m++)
+				{
+					for (int n = 0; n < 3; n++)
+					{	
+						System.out.print(subGrid[m][n] + "  ");
+					}
+					System.out.println();
+				}
+				System.out.println();
+				System.out.println();
+				///////////PRINTING//////////
+				
+				
+				if (CheckTheGrid(subGrid) != true)
+				{
+					return -2;
+				}
+				else
+				{
+					subGrid = new int[3][3];
+				}
+			}
+		}
+		
+		
+		// Iterating through each row to see if some digit appears two times
+		// If it does, the verify function will return -3 as per instructions
+		
+		for (int i = 0; i < rows.length; i++) {
+			char[] charArray = rows[i].toCharArray();
+			int[] integerArray = new int[9];
+
+			for (int j = 0; j < integerArray.length; j++) {
+				integerArray[j] = Character.getNumericValue(charArray[j]);
+			}
+
+			for (int k = 0; k < integerArray.length - 1; k++) {
+				for (int l = k + 1; l < integerArray.length; l++) {
+					if (integerArray[k] == integerArray[l])
+						return -3;
+				}
+			}
+		}
+		
+		// Iterating through each column to see if some digit appears two times
+		// If it does, the verify function will return -4 as per instructions
+		
+		for (int i = 0; i < columns.length; i++) {
+			char[] charArray = columns[i].toCharArray();
+			int[] integerArray = new int[9];
+
+			for (int j = 0; j < integerArray.length; j++) {
+				integerArray[j] = Character.getNumericValue(charArray[j]);
+			}
+
+			for (int k = 0; k < integerArray.length - 1; k++) {
+				for (int l = k + 1; l < integerArray.length; l++) {
+					if (integerArray[k] == integerArray[l])
+						return -4;
+				}
+			}
+		}
+		
+		// The candidate's solution was correct
 		return 0;
+	}
+	
+	
+	
+	
+	
+	///// HELPER METHODS ///////
+	
+	public boolean CheckSolutionLength(String solution)
+	{
+		if(solution.length() == 81)
+			return true;
+		else
+			return false;
+	}
+	
+	public String[] SplitRows(String string)
+	{
+		int startIndex = 0;
+		int endIndex = 9;
+		String[] strings = new String[9];
+		
+		for (int i = 0; i < 9; i++, startIndex += 9, endIndex += 9)
+		{
+			String row = string.substring(startIndex, endIndex);
+			strings[i] = row;
+		}
+		
+		return strings;
+	}
+	
+	
+	
+	public String[] SplitColumns(String string)
+	{
+		char[] charArray = string.toCharArray();
+		String[] columns = new String[9];
+		int columnNumber = 0;
+		boolean first = true;
+		
+		for(int i = 0; i < string.length(); i++, columnNumber++)
+		{
+			if(columnNumber == 9)
+			{
+				columnNumber = 0;
+				first = false;
+			}
+			
+			int integer = Character.getNumericValue(charArray[i]);
+			
+			if(first)
+				columns[columnNumber] = "" + String.valueOf(integer);
+			else
+				columns[columnNumber] = columns[columnNumber] + String.valueOf(integer);
+		}
+		
+		return columns;
+	}
+	
+	
+	
+	public boolean CheckTheGrid(int[][] grid)
+	{
+		
+		int[] array = new int[9];
+		int counter = 0;
+		
+		for (int i = 0; i < 3; i++)
+		{
+			for (int j = 0; j < 3; j++)
+			{	
+				array[counter] = grid[i][j];
+				counter++;
+			}
+		}
+		
+		for (int i = 0; i < array.length - 1; i++) {
+			for (int j = i + 1; j < array.length; j++) {
+				if (array[i] == array[j])
+					return false;
+			}
+		}
+		
+		return true;
 	}
 }
--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -7,8 +7,86 @@ import org.junit.Test;
 public class TestSudokuVerifier {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void TestIfTheCandidatesSolutionIs81() {
+		SudokuVerifier sudokuVerifier = new SudokuVerifier();
+		boolean check = sudokuVerifier.CheckSolutionLength("417369825632158947958724316825437169791586432346912758289643571573291684164875293");
+		assertTrue(check);
 	}
-
+	
+	@Test
+	public void TestIfTheRowsAreSplittedCorrectly() {
+		SudokuVerifier sudokuVerifier = new SudokuVerifier();
+		String[] actual = sudokuVerifier.SplitRows("417369825632158947958724316825437169791586432346912758289643571573291684164875293");
+		String expected = "417369825";
+		assertEquals(expected, actual[0]);
+	}
+	
+	@Test
+	public void TestIfTheColumnsAreSplittedCorrectly()	{
+		SudokuVerifier sudokuVerifier = new SudokuVerifier();
+		String[] actual = sudokuVerifier.SplitColumns("123456789912345678891234567789123456678912345567891234456789123345678912234567891");
+		String expected = "198765432";
+		assertEquals(expected, actual[0]);
+	}
+	
+	@Test
+	public void TestIfTheDigitsAreNot1_9()	{
+		SudokuVerifier sudokuVerifier = new SudokuVerifier();
+		int actual = sudokuVerifier.verify("103456789912345678891234567789123456678912345567891234456789123345678912234567891");
+		int expected = -1;
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void TestIfTheGridIsCheckedCorrectly()
+	{
+		SudokuVerifier sudokuVerifier = new SudokuVerifier();
+		int[][] grid = initializeTheGrid();
+		boolean check = sudokuVerifier.CheckTheGrid(grid);		
+		assertTrue(!check);
+	}
+	
+	@Test
+	public void TestIfDigitsAppearOnlyOnceInASubGrid()	{
+		SudokuVerifier sudokuVerifier = new SudokuVerifier();
+		int actual = sudokuVerifier.verify("123456789912345678891234567789123456678912345567891234456789123345678912234567891");
+		int expected = -2;
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void TestIfDigitsAppearOnlyOnceInEveryRow() {
+		SudokuVerifier sudokuVerifier = new SudokuVerifier();
+		int actual = sudokuVerifier.verify("417369825632158947958724316825437169791586432346912758289643571573295684164871293");
+		int expected = -3;
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void TestIfDigitsAppearOnlyOnceInEveryColumn() {
+		SudokuVerifier sudokuVerifier = new SudokuVerifier();
+		int actual = sudokuVerifier.verify("147369825632158947958724316825437169791586432346912758289643571573291684164875293");
+		int expected = -4;
+		assertEquals(expected, actual);
+	}
+	
+	
+	/////////// HELPER METHODS ////////////////
+	
+	public int[][] initializeTheGrid()
+	{
+		int[][] grid = new int[3][3];
+		int counter = 0;
+		
+		for (int i = 0; i < 3; i++)
+		{
+			for (int j = 0; j < 3; j++)
+			{	
+				grid[i][j] = counter;
+				counter++;
+			}
+		}
+		grid[0][1] = 0;
+		return grid;
+	}
 }
